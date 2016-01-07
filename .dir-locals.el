fff145d0ef5c63fc136c00718f3fab285c489126
Laszlo Andrasi <laszlo.andrasi@lensa.com>
2016-01-07 13:53:15 +0100

    improve test coverage a bit

---
+++ .dir-locals.el
@@ -0,0 +1,6 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((python-mode
+  (pytest-global-name . ".ve/bin/py.test")
+  (pytest-cmd-flags . "-v -s")))
