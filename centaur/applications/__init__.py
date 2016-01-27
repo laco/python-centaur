34b15cc8698037cc332c3f57b5b0c67ed79fc93f
Laszlo Andrasi <laszlo.andrasi@lensa.com>
2016-01-27 15:02:40 +0100

    applications refact

---
+++ centaur/applications/__init__.py
@@ -0,0 +1,33 @@
+import asyncio
+import functools
+
+
+class Adapter(object):
+    def __init__(self, application):
+        self.app = application
+        self.f_ = self.app.f_
+
+
+class Application(object):
+    def __init__(self, adapters=None):
+        adapters = adapters or {}
+        self.adapters = {aname: acls(self) for aname, acls in adapters.items()}
+        self.event_loop = self._get_event_loop()
+
+    def _get_event_loop(self):
+        policy = asyncio.get_event_loop_policy()
+        policy.get_event_loop().close()
+        event_loop = policy.new_event_loop()
+        policy.set_event_loop(event_loop)
+        return event_loop
+
+    def lookup_name(self, name):
+        adapter_name, attrib_name = name.split('.')
+        return getattr(self.adapters[adapter_name], attrib_name)
+
+    async def f_(self, fn_name, **kwargs):
+        coro = self.lookup_name(fn_name)
+        return await coro(**kwargs)
+
+    async def run_in_executor(self, fn, *args, **kwargs):
+        return await self.event_loop.run_in_executor(None, functools.partial(fn, *args, **kwargs))
---
+++ tests/test_centaur_applications.py
@@ -0,0 +1,62 @@
+import pytest
+from centaur.applications import Application, Adapter
+
+
+@pytest.fixture
+def sample_application():
+    return Application(adapters={'sample': SampleAdapter})
+
+
+class SampleAdapter(Adapter):
+    sample_attrib = 'sample'
+
+    async def sample_fn(self, p):
+        return p
+
+
+async def sample_coro():
+    return 'OK'
+
+
+def test_empty_application():
+    app = Application()
+    assert app is not None
+
+
+def test_sample_adapter(sample_application):
+    app = sample_application
+    assert 'sample' in app.adapters and isinstance(app.adapters['sample'], SampleAdapter)
+    assert app.adapters['sample'].app == app
+    assert app.lookup_name('sample.sample_attrib') == 'sample'
+
+
+def test_application_has_event_loop():
+    app = Application()
+    coro = sample_coro()
+    assert app.event_loop.run_until_complete(coro) is 'OK'
+
+
+def test_application_call_adapter_method(sample_application):
+    assert sample_application.event_loop.run_until_complete(sample_application.f_('sample.sample_fn', p='test')) == 'test'
+
+
+def test_application_with_two_adatpers():
+    class S1(Adapter):
+        async def fn_one(self, a):
+            return a * 2
+
+    class S2(Adapter):
+        async def fn_two(self, b):
+            return await self.f_('s1.fn_one', a=b)
+
+    app = Application(adapters={'s1': S1, 's2': S2})
+    assert app.event_loop.run_until_complete(app.f_('s2.fn_two', b=10)) == 20
+
+
+def test_application_run_in_executor():
+    def blocking_function(a, b):
+        import time
+        time.sleep(0.3)
+        return a + b
+    app = Application()
+    assert app.event_loop.run_until_complete(app.run_in_executor(blocking_function, a=10, b=10)) == 20
