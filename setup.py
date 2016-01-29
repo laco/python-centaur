4cdf627650059b716b8ea4b79f90cea950768f2c
Laszlo Andrasi <laszlo.andrasi@lensa.com>
2016-01-29 07:21:49 +0100

    fix applications import

--- setup.py
+++ setup.py
@@ -29,7 +29,7 @@ setup(
     author_email='mail@laszloandrasi.com',
     description='General Purpose Python3 Application Framework (Port-Adapter Architecture)',
     long_description=read_file('README.md') + '\n\n',
-    packages=['centaur', "centaur.contrib", 'centaur.datatypes', 'centaur.services'],
+    packages=['centaur', "centaur.contrib", 'centaur.datatypes', 'centaur.services', 'centaur.applications'],
     include_package_data=True,
     zip_safe=False,
     platforms='any',
