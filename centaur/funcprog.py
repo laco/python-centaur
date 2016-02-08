21da1030ae1b14516aaac69e06e8ca7239920867
Laszlo Andrasi <laszlo.andrasi@lensa.com>
2016-02-08 23:37:09 +0100

    funcprog function composition and a dummy lazy evaluation implementation

--- centaur/funcprog.py
+++ centaur/funcprog.py
@@ -19,3 +19,48 @@ def curry(fn):
             return partial(fn, *args, **kwargs)
 
     return wrapper
+
+
+class _Lazy(object):
+    def __init__(self, callable_, *args, **kwargs):
+        self._lazy_callable_ = callable_
+        self._lazy_args = args
+        self._lazy_kwargs = kwargs
+        self._lazy_called = False
+
+    def __eq__(self, other):
+        return self._val.__eq__(other)
+
+    @property
+    def _val(self):
+        if not self._lazy_called:
+            self._lazy_obj = self._lazy_callable_(*self._lazy_args, **self._lazy_kwargs)
+            self._lazy_called = True
+        return self._lazy_obj
+
+    # def __getattribute__(self, name):
+    #     try:
+    #         print(name)
+    #         return super().__getattribute__(name)
+    #     except AttributeError:
+    #         return self._lazy_val(name)
+
+
+def lazy(fn):
+    @wraps_w_signature(fn)
+    def wrapper(*args, **kwargs):
+        return _Lazy(fn, *args, **kwargs)
+    return wrapper
+
+
+def compose(*fns):
+    def wrapper(*args, **kwargs):
+        return compose(*fns[:-1])((fns[-1](*args, **kwargs)))
+
+    def identity(x):
+        return x
+
+    if len(fns) >= 1:
+        return wraps_w_signature(fns[-1])(wrapper)
+    else:
+        return identity
--- tests/test_funcprog.py
+++ tests/test_funcprog.py
@@ -1,9 +1,14 @@
-from centaur.funcprog import curry
+from centaur.funcprog import curry, lazy, compose
 
 
 @curry
 def sample_add(a, b):
     return a + b
+
+
+@curry
+def sample_multiply(a, b):
+    return a * b
 
 
 def test_curry_functions():
@@ -16,3 +21,45 @@ def test_curry_functions():
     assert add_2(1) == 3
     assert add_b(1) == 5
     assert sample_add(1)(1) == 2
+
+
+global_param = 0
+
+
+def _side_effect(param):
+    global global_param
+    global_param = param
+
+
+@lazy
+def lazy_add(a, b):
+    _side_effect(a + b)
+    return a + b
+
+
+def test_lazy_function_calls():
+    c = lazy_add(1, 1)
+    assert global_param == 0  #
+    assert c == 2  # evaluation v. side_effect called
+    assert global_param == 2
+
+
+def test_fn_composition():
+    double_plus_1 = compose(sample_add(1), sample_multiply(2))
+    assert double_plus_1(2) == 5
+    assert double_plus_1(0) == 1
+    assert double_plus_1(-1) == -1
+
+    double_neg_plus_1 = compose(sample_add(1), sample_multiply(-1), sample_multiply(2))
+
+    assert double_neg_plus_1(10) == -19
+    assert compose(sample_add(-1), sample_multiply)(2, 3) == 5
+    assert compose()(1) == 1  # identity
+
+    def factorial(n):
+        return compose(*[sample_multiply(i) for i in range(1, n + 1)])(1)
+
+    factorial_10 = 10 * 9 * 8 * 7 * 6 * 5 * 4 * 3 * 2
+    assert factorial(10) == factorial_10
+    assert factorial(2) == 2
+    assert factorial(3) == 6
