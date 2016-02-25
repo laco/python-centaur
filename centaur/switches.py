a1edd1db11a07e2372c34cbcfd736ab779d9b337
Laszlo Andrasi <laszlo.andrasi@lensa.com>
2016-02-25 14:34:17 +0100

    0.2.0 centaur.switches

---
+++ centaur/switches.py
@@ -0,0 +1,95 @@
+class SwitchStates(object):
+    GLOBAL_ENABLED = 'GLOBAL_ENABLED'
+    GLOBAL_DISABLED = 'GLOBAL_DISABLED'
+    SELECTIVE = 'SELECTIVE'
+
+
+class SwitchBoard(object):
+    def __init__(self):
+        self._switches = {}
+
+    def is_enabled(self, switch_name, *args, **kwargs):
+        try:
+            return self._switches[switch_name].is_enabled(*args, **kwargs)
+        except KeyError:
+            return False
+
+    def add_switch(self, name, *args, **kwargs):
+        self._switches[name] = Switch(name, *args, **kwargs)
+
+    def get_context(self, overwrite=None, *args, **kwargs):
+        def _overwrite(overwrite, ctx):
+            return {**ctx, **overwrite} if overwrite is not None else ctx
+        return _overwrite(
+            overwrite,
+            {name: self._switches[name].is_enabled(*args, **kwargs) for name in self._switches})
+
+
+class Switch(object):
+    def __init__(self, name,
+                 description=None,
+                 state=SwitchStates.GLOBAL_DISABLED,
+                 owner=None,
+                 expire_date=None,
+                 created_at=None, **kwargs):
+        self.name = name
+        self.description = description
+        self.state = state
+        self.owner = owner
+        self.expire_date = expire_date
+        self.created_at = created_at
+        self.conditions = kwargs
+
+    def is_enabled(self, *args, **kwargs):
+        if self.state == SwitchStates.GLOBAL_ENABLED:
+            return True
+        elif self.state == SwitchStates.GLOBAL_DISABLED:
+            return False
+
+    @classmethod
+    def from_dict(cls, data):
+        return cls(**data)
+
+    def to_dict(self):
+        keys = ['name', 'description', 'state', 'owner', 'expire_date', 'created_at', 'conditions']
+        return {k: getattr(self, k, None) for k in keys}
+
+
+class InMemorySwitchManager(object):
+    def __init__(self, initial_state):
+        self.state = initial_state
+
+    def load_from_database(self):
+        return {s: Switch.from_dict(self.state[s]) for s in self.state}
+
+    def store_to_database(self, switches):
+        self.state = {s: switches[s].to_dict() for s in switches}
+
+
+class NimoySchemaSwitchManager(object):  # noqa
+    def __init__(self, db_conn, schema_name):
+        self.db_conn = db_conn
+        self.schema_name = schema_name
+
+    def load_from_database(self):
+        return {s: Switch.from_dict(self.state[s]) for s in self._db_get_items()}
+
+    def _db_get_items(self):
+        return self.db_conn.scan(self.schema_name, _w=None, limit=9999)
+
+    def _db_put_item(self, switch_dict):
+        return self.db_conn.put_item(self.schema_name, switch_dict, overwrite=True)
+
+    def store_to_database(self, switches):
+        return {s: self._db_put_item(switches[s].to_dict()) for s in switches}
+
+
+class PersistentSwitchBoard(SwitchBoard):
+    def __init__(self, manager, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.manager = manager
+        self._switches = self.manager.load_from_database()
+
+    def add_switch(self, name, *args, **kwargs):
+        super().add_switch(name, *args, **kwargs)
+        self.manager.store_to_database(self._switches)
---
+++ tests/test_switches.py
@@ -0,0 +1,38 @@
+from centaur.switches import SwitchBoard, SwitchStates, PersistentSwitchBoard, InMemorySwitchManager
+
+
+def test_create_empty_switchboard():
+    board = SwitchBoard()
+    assert board is not None
+    assert board.is_enabled('MISSING_FEATURE_DEFAULTS_TO_FALSE') is False
+
+
+def test_switchboard_with_test_switch():
+    board = SwitchBoard()
+    board.add_switch('TEST_SWITCH',
+                     description='This is the description',
+                     state=SwitchStates.GLOBAL_ENABLED)
+    board.add_switch('DISABLED_SWITCH',
+                     state=SwitchStates.GLOBAL_DISABLED)
+    assert board.is_enabled('TEST_SWITCH') is True
+    assert board.is_enabled('DISABLED_SWITCH') is False
+
+
+def test_switchboard_all_switches():
+    board = SwitchBoard()
+    board.add_switch('ENABLED_TEST1', state=SwitchStates.GLOBAL_ENABLED)
+    board.add_switch('DISABLED_TEST2')
+
+    assert board.get_context() == {'ENABLED_TEST1': True, 'DISABLED_TEST2': False}
+    assert board.get_context(overwrite={'DISABLED_TEST2': True}) == {'ENABLED_TEST1': True, 'DISABLED_TEST2': True}
+
+
+def test_inmemory_persistent_switches():
+    manager = InMemorySwitchManager({
+        'TEST1': {'name': 'TEST1', 'state': SwitchStates.GLOBAL_ENABLED}
+    })
+    board = PersistentSwitchBoard(manager=manager)
+    board.add_switch('TEST2')
+    assert board.is_enabled('TEST1') is True
+    assert 'TEST2' in manager.state
+    assert manager.state['TEST2']['state'] == SwitchStates.GLOBAL_DISABLED
