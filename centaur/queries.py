84f0cbd5650f4080f270d27836f6fe472665da19
Laszlo Andrasi <laszlo.andrasi@lensa.com>
2016-03-07 14:04:39 +0100

    centaur.queries parse_wt implementation with TDD

---
+++ centaur/queries.py
@@ -0,0 +1,155 @@
+import operator
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+class WQuery(object):
+    pass
+
+
+class PrimitiveWQuery(WQuery):
+    def __init__(self, selector, argument):
+        self.selector = selector
+        self.argument = argument
+
+    def select(self, value):
+        return value.get(self.selector)
+
+    def check(self, value):
+        try:
+            return self._operator(self.select(value), self.argument)
+        except TypeError as e:
+            logger.warn(e)
+            return False
+
+    def __repr__(self):
+        return "('{}', '{}', {})".format(self._tag, self.selector, repr(self.argument))
+
+
+class CompositeWQuery(WQuery):
+    def __init__(self, *subqueries):
+        self.subqueries = subqueries
+
+    def __repr__(self):
+        def _space_indent(spaces, s):
+            return '\n'. join([(' ' * spaces) + line for line in s.split('\n')])
+
+        return "('{}',\n{})".format(
+            self._tag,
+            _space_indent(1,
+                          ',\n'.join([repr(s) for s in self.subqueries])))
+
+
+class WEq(PrimitiveWQuery):
+    _operator = operator.eq
+    _tag = 'eq'
+
+
+class WNeq(PrimitiveWQuery):
+    _operator = operator.ne
+    _tag = 'neq'
+
+
+class WLt(PrimitiveWQuery):
+    _operator = operator.lt
+    _tag = 'lt'
+
+
+class WGt(PrimitiveWQuery):
+    _operator = operator.gt
+    _tag = 'gt'
+
+
+class WLte(PrimitiveWQuery):
+    _operator = operator.le
+    _tag = 'lte'
+
+
+class WGte(PrimitiveWQuery):
+    _operator = operator.ge
+    _tag = 'gte'
+
+
+class WIn(PrimitiveWQuery):
+    _tag = 'in'
+
+    def check(self, value):
+        return self.select(value) in self.argument
+
+
+class WNin(PrimitiveWQuery):
+    _tag = 'nin'
+
+    def check(self, value):
+        return self.select(value) not in self.argument
+
+
+class WContains(PrimitiveWQuery):
+    _operator = operator.contains
+    _tag = 'contains'
+
+
+class WNContains(WContains):  # FIXME: better solution for negation!
+    _tag = 'ncontains'
+
+    def check(self, value):
+        return not super().check(value)
+
+
+class WStartswith(PrimitiveWQuery):
+    _tag = 'startswith'
+
+    def check(self, value):
+        return self.select(value).startswith(self.argument)
+
+
+class WEndswith(PrimitiveWQuery):
+    _tag = 'endswith'
+
+    def check(self, value):
+        return self.select(value).endswith(self.argument)
+
+
+class WAnd(CompositeWQuery):
+    _tag = 'and'
+
+    def check(self, value):
+        return all([s.check(value) for s in self.subqueries])
+
+
+class WNot(CompositeWQuery):
+    _tag = 'not'
+
+    def check(self, value):
+        return not self.subqueries[0].check(value)
+
+
+class WOr(CompositeWQuery):
+    _tag = 'or'
+
+    def check(self, value):
+        return any([s.check(value) for s in self.subqueries])
+
+
+_primitive_queries = {
+    'eq': WEq, 'neq': WNeq,
+    'lt': WLt, 'gt': WGt,
+    'lte': WLte, 'gte': WGte,
+    'in': WIn, 'nin': WNin,
+    'contains': WContains, 'ncontains': WNContains,
+    'startswith': WStartswith, 'endswith': WEndswith,
+}
+
+_composite_queries = {
+    'and': WAnd,
+    'not': WNot,
+    'or': WOr,
+}
+
+
+def parse_wt(wt):
+    if wt[0] in _primitive_queries:
+        return _primitive_queries[wt[0]](*wt[1:])
+    elif wt[0] in _composite_queries:
+        return _composite_queries[wt[0]](*[parse_wt(swt) for swt in wt[1:]])
---
+++ tests/test_queries.py
@@ -0,0 +1,97 @@
+import pytest
+from centaur.queries import parse_wt
+
+
+@pytest.mark.parametrize(
+    'w, value, result', [
+        (('eq', 'name', 'Sample'), {'name': 'Sample'}, True),
+        (('eq', 'name', 'Sample'), {'name': 'NOOOOO'}, False),
+
+        (('neq', 'name', 'Sample'), {'name': 'NOOOO'}, True),
+        (('neq', 'name', 'Sample'), {'name': 'Sample'}, False),
+
+        (('lt', 'age', 10), {'age': 9}, True),
+        (('lt', 'age', 10), {'age': 10}, False),
+
+        (('gt', 'age', 10), {'age': 11}, True),
+        (('gt', 'age', 10), {'age': 10}, False),
+
+        (('lte', 'age', 10), {'age': 9}, True),
+        (('lte', 'age', 10), {'age': 10}, True),
+        (('lte', 'age', 10), {'age': 11}, False),
+
+        (('gte', 'age', 10), {'age': 11}, True),
+        (('gte', 'age', 10), {'age': 10}, True),
+        (('gte', 'age', 10), {'age': 9}, False),
+
+        (('in', 'category', ['A', 'B']), {'category': 'A'}, True),
+        (('in', 'category', ['A', 'B']), {'category': 'B'}, True),
+        (('in', 'category', ['A', 'B']), {'category': 'C'}, False),
+
+        (('nin', 'category', ['A', 'B']), {'category': 'A'}, False),
+        (('nin', 'category', ['A', 'B']), {'category': 'B'}, False),
+        (('nin', 'category', ['A', 'B']), {'category': 'C'}, True),
+
+        (('contains', 'tags', 'A'), {'tags': ['A', 'B']}, True),
+        (('contains', 'tags', 'B'), {'tags': ['A', 'B']}, True),
+        (('contains', 'tags', 'C'), {'tags': ['A', 'B']}, False),
+
+        (('ncontains', 'tags', 'A'), {'tags': ['A', 'B']}, False),
+        (('ncontains', 'tags', 'B'), {'tags': ['A', 'B']}, False),
+        (('ncontains', 'tags', 'C'), {'tags': ['A', 'B']}, True),
+
+        (('startswith', 'name', 'Example'), {'name': 'Example Name'}, True),
+        (('startswith', 'name', 'Example'), {'name': 'Not-Example Name'}, False),
+
+        (('endswith', 'name', 'Example'), {'name': 'First Example'}, True),
+        (('endswith', 'name', 'Example'), {'name': 'Second Example End'}, False),
+
+    ])
+def test_parse_wt_simple(w, value, result):
+    wq = parse_wt(w)
+    assert wq.check(value) is result
+
+
+def test_parse_wt_and_query():
+    w = ('and',
+         ('eq', 'name', 'Sample'),
+         ('eq', 'age', 18))
+    wq = parse_wt(w)
+    assert wq.check({}) is False
+    assert wq.check({'name': 'XXX', 'age': 18}) is False
+    assert wq.check({'name': 'Sample', 'age': 17}) is False
+    assert wq.check({'name': 'Sample', 'age': 18}) is True
+
+
+def test_parse_wt_not():
+    w = ('not',
+         ('eq', 'name', 'Sample'))
+    wq = parse_wt(w)
+    assert wq.check({'name': 'Sample'}) is False
+    assert wq.check({'name': 'Not Sample'}) is True
+
+
+def test_parse_wt_or():
+    w = ('or',
+         ('eq', 'title', 'Example'),
+         ('gt', 'length', 100))
+    wq = parse_wt(w)
+    assert wq.check({'title': 'Example'}) is True
+    assert wq.check({'length': 101}) is True
+    assert wq.check({'title': 'Not Example', 'length': 0}) is False
+
+
+def test_query_repr():
+    w = ('and',
+         ('or',
+          ('eq', 'name', 'Example1'),
+          ('eq', 'name', 'Example2')),
+         ('gt', 'age', 18))
+    wq = parse_wt(w)
+    assert repr(wq) == """
+('and',
+ ('or',
+  ('eq', 'name', 'Example1'),
+  ('eq', 'name', 'Example2')),
+ ('gt', 'age', 18))
+""".strip()
